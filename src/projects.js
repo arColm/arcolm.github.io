fdc23d53125111c40de1ac7273df71aabc11399b
arColm <jacob.leung@mail.mcgill.ca>
2025-04-15 22:51:54 -0400

    change link and pic

--- src/projects.js
+++ src/projects.js
@@ -281,7 +281,7 @@ function addClickEventToButtons() {
                     using deferred rendering techniques, <b>IFFT and JONSWAP generated spectrum</b> for realistic water,
                     <b>raymarched volumetric clouds</b>, <b>cascaded shadow mapping</b>, mie and rayleigh reflections for the sun and skybox.\n
                     The focus was learning Vulkan, and also to try more advanced rendering techniques such as g-buffers.
-                    A video can be found <a href="https://www.youtube.com/watch?v=ms9te2xd34Q"> here</a>.\n
+                    A video can be found <a href="https://www.youtube.com/watch?v=KDSO6FluAOU"> here</a>.\n
                     The repository can be found <a href="https://github.com/arColm/grass_renderer"> here</a>.\n
 
                     `;
